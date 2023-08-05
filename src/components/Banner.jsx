97b86c1523bbf9985555bdff6daf8dd530c73957
5xDiego <schindelediego@gmail.com>
2023-08-05 22:09:58 +0200

    added dist

--- src/components/Banner.jsx
+++
@@ -1,7 +0,0 @@
-export default function Banner(){
-    return(
-        <div className="navbar w-full bg-neutral shadow-2xl flex flex-col">
-            <h1 className="text-3xl lg:text-6xl text-neutral-content lg:pb-5 lg:pr-5"> Schau das Video  </h1>
-        </div>
-    );
-}
